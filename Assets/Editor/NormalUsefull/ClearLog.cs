a4bfab776580c5f36487b5487cc34ad06d8ad2eb
iningwei <iningwei@outlook.com>
2019-02-25 10:27:43 +0800

    editor clear log

---
+++ Assets/Editor/NormalUsefull/ClearLog.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+public class ClearLog : MonoBehaviour
+{
+    /// <summary>
+    /// ClearLog的具体方法可以用Reflector或者SLSpy查看Unity源码。不同版本有差异
+    /// </summary>
+    [MenuItem("工具/ClearLog")]
+    public static void DoClearLog()
+    {
+        var assembly = Assembly.GetAssembly(typeof(UnityEditor.ActiveEditorTracker));
+        var type = assembly.GetType("UnityEditorInternal.LogEntries");
+        var method = type.GetMethod("Clear");
+        method.Invoke(new object(), null);
+    }
+}
