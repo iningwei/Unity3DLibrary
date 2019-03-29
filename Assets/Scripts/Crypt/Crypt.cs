f1f797fe860a3682caafc61bc92ee779386c2d4c
iningwei <iningwei@outlook.com>
2019-03-29 19:07:05 +0800

    add crypt.cs

---
+++ Assets/Scripts/Crypt/Crypt.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crypt
+{
+    #region 字符串加解密
+    //http://www.jb51.net/article/58442.htm
+    static string TextEncrypt(string content, string secretKey)
+    {
+        char[] data = content.ToCharArray();
+        char[] key = secretKey.ToCharArray();
+        for (int i = 0; i < data.Length; i++)
+        {
+            data[i] ^= key[i % key.Length];
+        }
+        return new string(data);
+    }
+    private string TextDecrypt(char[] data, string secretKey)
+    {
+        char[] key = secretKey.ToCharArray();
+        for (int i = 0; i < data.Length; i++)
+        {
+            data[i] ^= key[i % key.Length];
+        }
+        return new string(data);
+    }
+    #endregion
+}
