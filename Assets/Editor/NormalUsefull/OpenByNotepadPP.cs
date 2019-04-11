e6cb528e64dc032a5d896abdf6cfa556ec51f4c0
iningwei <iningwei@outlook.com>
2019-04-11 17:56:50 +0800

    notepad++ now can open .shader file from unity3d

--- Assets/Editor/NormalUsefull/OpenByNotepadPP.cs
+++ Assets/Editor/NormalUsefull/OpenByNotepadPP.cs
@@ -6,7 +6,7 @@ public class OpenByNotepadPP : MonoBehaviour
 {
     //http://docs.unity3d.com/ScriptReference/MenuItem.html
     [MenuItem("Assets/使用Notepad++打开", false, 1)]
-    static void  openByNotepadPP()
+    static void openByNotepadPP()
     {
         if (Selection.objects.Length != 1)//由于文本文件不属于GameObject类型，因此这里使用Selection.gameObjects是无法获取选择的文本文件的
         {
@@ -15,11 +15,13 @@ public class OpenByNotepadPP : MonoBehaviour
         }
         UnityEngine.Object obj = Selection.objects[0];
         string path = Application.dataPath + AssetDatabase.GetAssetPath(obj).Substring(6);
-        if (!(path.EndsWith(".cs") || path.EndsWith(".txt") || path.EndsWith(".bin") || path.EndsWith(".xml")))
+        if (!(path.EndsWith(".cs") || path.EndsWith(".txt")
+           || path.EndsWith(".bin") || path.EndsWith(".xml")
+           || path.EndsWith(".shader")))
         {
-            UnityEngine.Debug.LogError("you can only open .cs .txt .bin .xml file");
+            UnityEngine.Debug.LogError("you can only open .cs .txt .bin .xml .shader file");
             return;
-        }         
+        }
         UnityEngine.Debug.Log(path + ",opened");
         //Process.Start(path);//使用默认打开方式打开文本文件
         Process.Start("notepad++.exe", path);//使用notepad++打开，需要机器安装了该软件
