602a723d6f4e4dbdad6a4019c9193edc62b7ab5f
iningwei <iningwei@outlook.com>
2020-03-19 17:34:21 +0800

    Color extention

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    
+    void Start()
+    { 
+        Color rgb2 = new Color(69f / 255, 139f / 255f, 116f / 255f);
+        string xx=rgb2.RGBToHex();
+        Debug.Log(xx);
+    }
+
+  
+}
