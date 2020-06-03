1d5a4a7738562cf6793132c40f4f443fc89c0f1c
iningwei <iningwei@outlook.com>
2020-06-03 19:32:31 +0800

    add bytescrypt

--- Assets/Scripts/Crypt/Crypt.cs
+++ Assets/Scripts/Crypt/Crypt.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class Crypt
 {
     #region 字符串加解密
     //http://www.jb51.net/article/58442.htm
-    static string TextEncrypt(string content, string secretKey)
+    public static string TextEncrypt(string content, string secretKey)
     {
         char[] data = content.ToCharArray();
         char[] key = secretKey.ToCharArray();
@@ -16,7 +17,7 @@ public class Crypt
         }
         return new string(data);
     }
-    private string TextDecrypt(char[] data, string secretKey)
+    public static string TextDecrypt(char[] data, string secretKey)
     {
         char[] key = secretKey.ToCharArray();
         for (int i = 0; i < data.Length; i++)
@@ -24,6 +25,44 @@ public class Crypt
             data[i] ^= key[i % key.Length];
         }
         return new string(data);
+    }
+    #endregion
+
+    #region 二进制加解密
+    public void FileEncrypt(string filePath, string key)
+    {
+        var data_bytes = File.ReadAllBytes(filePath);
+        File.WriteAllBytes(filePath, BytesEncrypt(data_bytes, key));
+    }
+
+    public byte[] BytesEncrypt(byte[] data_bytes, string key)
+    {
+        if (key.Trim() == "")
+        {
+            Debug.LogError("error,key can not be empty!");
+        }
+        var key_bytes = System.Text.Encoding.UTF8.GetBytes(key);
+        for (int i = 0; i < data_bytes.Length; i++)
+        {
+            data_bytes[i] = (byte)(data_bytes[i] ^ key_bytes[i % key_bytes.Length]);
+        }
+        return data_bytes;
+    }
+
+    public byte[] BytesDecrypt(byte[] data_bytes, string key)
+    {
+        if (key.Trim() == "")
+        {
+            Debug.LogError("error,key can not be empty!");
+
+        }
+        var key_bytes = System.Text.Encoding.UTF8.GetBytes(key);
+        for (int i = 0; i < data_bytes.Length; i++)
+        {
+            data_bytes[i] = (byte)(data_bytes[i] ^ key_bytes[i % key_bytes.Length]);
+        }
+
+        return data_bytes;
     }
     #endregion
 }
